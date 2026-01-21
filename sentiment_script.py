7d3d5ebb2f8be7a7037cd55e27f221de65c84355
Azzurre <37148783+Azzurre@users.noreply.github.com>
2026-01-21 00:13:47 +0000

    Add imports for sentiment analysis and text processing in sentiment_script.py

--- sentiment_script.py
+++ sentiment_script.py
@@ -1,4 +1,11 @@
 import nltk
+import pandas as pd
+
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
 
 nltk.download('all')
 print("All NLTK data packages have been downloaded.")
